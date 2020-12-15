08863ac53aefe3b87e43ab81ddff11d1df0e5012
MPanda <HamHwong@gmail.com>
2020-12-15 15:35:27 +0800

    Modified Port for test

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-07 10:55:23
- * @LastEditTime: 2020-12-09 13:07:09
+ * @LastEditTime: 2020-12-15 15:35:04
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /MPANDA.SUTDIO/config.js
@@ -12,7 +12,7 @@ const static = static_(
   path.join(__dirname, './static')
 )
 // Server Port Settings
-var port = '12345';
+var port = '8100';
 // MongoDB Settings
 var mongoDB = {
   host:'45.77.185.26',
