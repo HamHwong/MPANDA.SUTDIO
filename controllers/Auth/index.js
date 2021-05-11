31b62cc8aaf9a2e5903f73831dadaa43d8aa3f9e
MPanda <HamHwong@gmail.com>
2021-05-11 18:03:56 +0800

    check

--- controllers/Auth/index.js
+++ controllers/Auth/index.js
@@ -14,5 +14,12 @@ router.post('/auth/login',async(ctx,next)=>{
   }
   ctx.send(result)
 })
+router.get('/oauth2/wechat/check',async(ctx,next)=>{ 
+  //ctx.send(new response(`result`) )
+  const APPID = 'wx1945f85c362dd76f'
+  const REDIRECT_URI = 'localhost:8080'
+  const path = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=SCOPE&state=STATE#wechat_redirect`
+  ctx.response.redirect(path);
+})
 
 module.exports = router
