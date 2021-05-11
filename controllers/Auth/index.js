a08009912a5a1069523ea5f60e7d4d67de463834
MPanda <HamHwong@gmail.com>
2021-05-11 19:34:22 +0800

    crypto

--- controllers/Auth/index.js
+++ controllers/Auth/index.js
@@ -1,39 +1,41 @@
-
-let router = require('koa-router')() 
+let router = require('koa-router')()
 const User = require('../../model/User')
 var response = require('../../model/Response')
-let {AuthService} = require('../../services/auth')
-var crypto = require('crypto');
-var querystring = require('querystring');
-router.post('/auth/login',async(ctx,next)=>{
+let { AuthService } = require('../../services/auth')
+var crypto = require('crypto')
+var querystring = require('querystring')
+router.post('/auth/login', async (ctx, next) => {
   var user = User.Convert(ctx.request.body)
-  var loginUser = await AuthService.login(user.account,user.password) 
+  var loginUser = await AuthService.login(user.account, user.password)
   var result = null
-  if(loginUser){
-    result = new response(loginUser) 
-  }else{
+  if (loginUser) {
+    result = new response(loginUser)
+  } else {
     throw new Error('验证失败')
   }
   ctx.send(result)
 })
-router.get('/oauth2/wechat/oauth2',async(ctx,next)=>{ 
+router.get('/oauth2/wechat/oauth2', async (ctx, next) => {
   //ctx.send(new response(`result`) )
   const APPID = 'wx1945f85c362dd76f'
-  const REDIRECT_URI = 'mpanda.studio' 
+  const REDIRECT_URI = 'mpanda.studio'
   const path = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=SCOPE&state=STATE#wechat_redirect`
-  ctx.response.redirect(path);
+  ctx.response.redirect(path)
 })
-router.get('/oauth2/wechat/check',async(ctx,next)=>{  
+router.get('/oauth2/wechat/check', async (ctx, next) => {
   //signature=6a401ffdd33d2f1ce0bf983b9bae3a4a3fe0e312
   //echostr=7174475387871686655
   //timestamp=1620731823
   //nonce=137469566
   // querystring.parse()
   // console.log(ctx.request.query)
-  var echostr = ctx.request.query.echostr||''
-  console.log(echostr)
-  // var sha1 = crypto.createHash('sha1').update(str).digest('hex').toUpperCase();
-  ctx.send(echostr)
+  const { signature='', echostr='', timestamp='', nonce='' } = ctx.request.query
+  var token = 'mpandastudio'
+  var arr = [token,timestamp,nonce].sort()
+  var tmpStr = arr.join('')
+  console.log('aaa', echostr)
+  var sha1 = crypto.createHash('sha1').update(tmpStr).digest('hex').toUpperCase();
+  ctx.send(sha1)
 })
 
 module.exports = router
