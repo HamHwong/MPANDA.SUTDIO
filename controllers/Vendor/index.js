fff34ed3e2d7725760c6a70cbb1191d8f543d16c
MPanda <HamHwong@gmail.com>
2021-05-28 17:40:22 +0800

    fix

--- controllers/Vendor/index.js
+++ controllers/Vendor/index.js
@@ -13,60 +13,88 @@ var response = require('../../model/Response')
 let router = require('koa-router')()
 
 router.get('/Vendor/Bing/GetDailyBG', async (ctx, next) => {
-    var options = {
-        host: 'cn.bing.com',
-        path: '/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN',
-        method: 'GET',
-    } 
-    await new Promise((resolve,rej)=>{
-        https.get(options, (res) => {
-            var rawData = ''
-            res.setEncoding('utf8');
-            res.on('data', (d) => {
-                rawData += d
-            })
-            res.on('end', () => {
-                try {
-                    const parsedData = JSON.parse(rawData);
-                    ctx.send(new response(parsedData))
-                    resolve(parsedData)
-                } catch (e) {
-                    console.error(e.message);
-                    rej(e.message)
-                }
-            })
-        }).end()
-    }) .catch(e=>{
-        
-    })
+  var options = {
+    host: 'cn.bing.com',
+    path: '/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN',
+    method: 'GET',
+  }
+  await new Promise((resolve, rej) => {
+    https
+      .get(options, (res) => {
+        var rawData = ''
+        res.setEncoding('utf8')
+        res.on('data', (d) => {
+          try {
+            rawData += d
+          } catch (e) {
+            console.error(e.message)
+            rej(e.message)
+          }
+        })
+        res.on('error', function () {
+          try {
+            res.destory()
+          } catch (e) {
+            rej(e.message)
+          } finally {
+            ctx.utils.logger.info(String(e.stack))
+          }
+        })
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData)
+            ctx.send(new response(parsedData))
+            resolve(parsedData)
+          } catch (e) {
+            console.error(e.message)
+            rej(e.message)
+          }
+        })
+      })
+      .end()
+  }).catch((e) => {})
 })
-router.get('/Vendor/Other/GetDailyWords',async (ctx,next)=>{
-    //http://api.lkblog.net/ws/api.php
-    var options = {
-        host: 'api.lkblog.net',
-        path: '/ws/api.php',
-        method: 'GET',
-    } 
-    await new Promise((resolve,rej)=>{
-        http.get(options,(res)=>{
-            var rawData = ''
-            res.on('data', (d) => {
-                rawData += d
-            })
-            res.on('end', () => {
-                try {
-                    const parsedData = JSON.parse(rawData);
-                    // console.log(parsedData);
-                    ctx.send(new response(parsedData))
-                    resolve(parsedData)
-                } catch (e) {
-                    console.error(e.message);
-                    rej(e.message)
-                }
-            })
-        }).end()
-    }).catch(e=>{
-        
-    })
+router.get('/Vendor/Other/GetDailyWords', async (ctx, next) => {
+  //http://api.lkblog.net/ws/api.php
+  var options = {
+    host: 'api.lkblog.net',
+    path: '/ws/api.php',
+    method: 'GET',
+  }
+  await new Promise((resolve, rej) => {
+    http
+      .get(options, (res) => {
+        var rawData = ''
+        res.on('data', (d) => {
+          try {
+            rawData += d
+          } catch (e) {
+            console.error(e.message)
+            rej(e.message)
+          }
+        })
+        res.on('error', function () {
+          try {
+            res.destory()
+          } catch (e) {
+            rej(e.message)
+          } finally {
+            ctx.utils.logger.info(String(e.stack))
+          }
+        })
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData)
+            // console.log(parsedData);
+            ctx.send(new response(parsedData))
+            resolve(parsedData)
+          } catch (e) {
+            console.error(e.message)
+            rej(e.message)
+          }
+        })
+      })
+      .end()
+  }).catch((e) => {})
 })
 module.exports = router
