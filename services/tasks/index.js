1546c99ec0d4cb7b66c5eb65b079846745366e93
MPanda <HamHwong@gmail.com>
2021-06-13 23:25:56 +0800

    'fix something

--- services/tasks/index.js
+++ services/tasks/index.js
@@ -2,8 +2,8 @@ const { IService } = require('../IServices')
 const { Task } = require('../../model/Task')
 const { isNull } = require('../../utils/common')
 const TaskModel = require('../../model/Task')
-const enums = require('../../model/Task/enums') 
-const logger = require('../../utils/log4js')  
+const enums = require('../../model/Task/enums')
+const logger = require('../../utils/log4js')
 var mongodb = require('mongodb')
 var ObjectID = mongodb.ObjectID
 class Tasks extends IService {
@@ -22,37 +22,47 @@ class Tasks extends IService {
     // 文章中的任务
     const tasks = await this.getTasksFromArticle(Article)
     // 已生成的任务
-    const existedTasks = await this.getTasksByArticle(Article._id) 
+    const existedTasks = await this.getTasksByArticle(Article._id)
     // 生成新任务
-    const newTasks = tasks.filter(task=>!existedTasks.find(existTask=>existTask.content.trim()===task.content.trim()))
-    const oldTasks = tasks.filter(task=>existedTasks.find(existTask=>existTask.content.trim()===task.content.trim()))
-    // console.log('existedTasks',existedTasks)
-    // console.log('newTasks',newTasks)
-    // console.log('oldTasks',oldTasks)
-    newTasks.map(async newTask=>{
+    let newTasks = tasks.filter(
+      (task) =>
+        !existedTasks.find(
+          (existTask) => existTask.content.trim() === task.content.trim()
+        )
+    )
+    newTasks = newTasks.filter((i, index) => newTasks.findIndex(j=>j.content.trim()===i.content.trim()) === index)
+    let oldTasks = tasks.filter((task) =>
+      existedTasks.find(
+        (existTask) => existTask.content.trim() === task.content.trim()
+      )
+    ) 
+    oldTasks = oldTasks.filter((i, index) => oldTasks.findIndex(j=>j.content.trim()===i.content.trim()) === index)
+    // console.log('existedTasks', existedTasks)
+    // console.log('newTasks', newTasks)
+    newTasks.map(async (newTask) => {
       await this.Create(newTask)
     })
-    // 更新老任务 
-    oldTasks.filter(async oldTask=>{
+    // 更新老任务
+    oldTasks.filter(async (oldTask) => {
       // existedTasks 包含_id
-      var task = existedTasks.filter(existedTask=>[
-        existedTask.content.trim()===oldTask.content.trim()
+      var task = existedTasks.filter((existedTask) => [
+        existedTask.content.trim() === oldTask.content.trim(),
       ])
-      if(task.length>0){
-        await this.Update(task[0]._id,{status:oldTask.status})
+      if (task.length > 0) {
+        await this.Update(task[0]._id, { status: oldTask.status })
       }
     })
-    // 对于从内容中被删除的老的任务，不做更新 
-  } 
-  async getTasksByArticle(_id){ 
-    const tasks = await this.List({relatedId:_id}) 
-    return tasks.map(i=>TaskModel.Convert(i))
+    // 对于从内容中被删除的老的任务，不做更新
   }
-  async getTasksFromArticle(Article) { 
+  async getTasksByArticle(_id) {
+    const tasks = await this.List({ relatedId: _id })
+    return tasks.map((i) => TaskModel.Convert(i))
+  }
+  async getTasksFromArticle(Article) {
     if (!isNull(Article)) {
       var result = []
       var regex = /(\* \[(x| )?\] *.+)/gi
-      var tasks = Article.content.match(regex) 
+      var tasks = Article.content.match(regex)
       if (tasks.length > 0) {
         tasks.map(async (taskString) => {
           var taskArr = taskString.split(' ')
@@ -103,7 +113,7 @@ class Tasks extends IService {
   // async updateTaskInArticle(task){
   //   this.tableName= 'Articles'
   //   const Article = await this.Get(task.relatedId)
-  //   this.tableName= 'Tasks' 
+  //   this.tableName= 'Tasks'
   //   if(Article&&task.relatedId){
   //     return await this.Update(task.relatedId,{content:Article.content.replaceAll(task.revertToString(),task.toString())})
   //   }
